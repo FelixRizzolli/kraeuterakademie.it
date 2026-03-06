9ad93c30d7a38c6d9e4b4c9f36ea9c00bda9fc81
Felix Rizzolli <felix.rizzolli@outlook.de>
2026-03-06 15:33:37 +0000

    chore(devcontainer): adjust database init script

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Split DATABASE_NAMES by comma and create each database
-IFS=',' read -ra DBS <<< "$DATABASE_NAMES"
-for db in "${DBS[@]}"; do
-  echo "Creating database: $db"
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE "$db";
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE "$POSTGRES_DB";
 EOSQL
-done
